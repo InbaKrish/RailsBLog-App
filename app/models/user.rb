3d2a5ee3188d7922e38c8932ec51470c12b4e30f
Inba_Krish <inbakrish007@gmail.com>
2021-06-13 11:56:42 +0530

    Added one-to-many association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+    has_many :articles
+
     validates :user_name, presence: true, uniqueness: { case_sensitive: false },  length: {minimum: 3, maximum: 80}
 
     EMAIL_VALID_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
