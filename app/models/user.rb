45280099bc3914fe29e98751daad9d0976c456bc
Inba_Krish <inbakrish007@gmail.com>
2021-06-13 13:10:44 +0530

    Added auth using BCrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
     EMAIL_VALID_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, uniqueness: { case_sensitive: false },  length: {minimum: 3, maximum: 80},
                 format: {with: EMAIL_VALID_REGEX}
+
+    has_secure_password
 end
