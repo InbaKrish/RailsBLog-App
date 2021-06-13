c2ca9070a5a7a11b7436bc32460c1bc6ea3aa241
Inba_Krish <inbakrish007@gmail.com>
2021-06-13 13:43:12 +0530

    Added a signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#home"
-  resources :articles, except: [:destroy]
 
+  #article routes
+  resources :articles, except: [:destroy]
   delete 'articles/:id/delete' => 'articles#destroy', as: 'articles_delete'
   get '/articles/:id/delete' => 'articles#destroy'
+
+  # user routes
+  get 'signup' => 'users#new'
+  resources :users, except: [:new]
 end
