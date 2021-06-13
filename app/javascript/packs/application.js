7db012a6133135a33da42e150c5bc0f6ce33e052
Inba_Krish <inbakrish007@gmail.com>
2021-06-13 10:18:51 +0530

    Added user validations

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 //= require jquery
-//= require jquery-ui
-//= require popper
-//= require bootstrap-sprockets
-//= require rails-ujs
-//= require activestorage
 //= require turbolinks
+//= require jquery.turbolinks
+//= require jquery_ujs
+//= require foundation
 //= require_tree .
 
 
