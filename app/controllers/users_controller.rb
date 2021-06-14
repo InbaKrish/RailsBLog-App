1383e422d21ae0e05b35529b11a02c501d95d080
Inba_Krish <inbakrish007@gmail.com>
2021-06-14 19:37:10 +0530

    .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "#{@user.user_name} Signed Up Successfully..."
             redirect_to articles_path
         else
