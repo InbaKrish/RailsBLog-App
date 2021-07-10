43550b031b68db678772c77c5d13ddd1989dd744
Inba_Krish <inbakrish007@gmail.com>
2021-07-10 08:14:48 +0530

    Cleaned migration files

--- db/migrate/20210614041306_add_users_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddUsersToComments < ActiveRecord::Migration[6.1]
-  def change
-    add_column :comments, :user, :int
-  end
-end
--- db/migrate/20210614055019_add_instaurl_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddInstaurlToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :instaurl, :string
-  end
-end
